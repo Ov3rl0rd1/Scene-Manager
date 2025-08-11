c7121061ba0f4a293e7050787a644e61a86d2a73
Overlord <nrbudanov1123@gmail.com>
2025-08-11 21:33:07 +0300

    Update hierarchy

--- Assets/Scripts/SceneInstaller.cs
+++ Assets/Scripts/SceneInstaller.cs
@@ -4,13 +4,11 @@ using Zenject;
 public class SceneInstaller : MonoInstaller
 {
     [SerializeField] private Player _player;
-    [SerializeField] private GameState _gameState;
     [SerializeField] private LevelLoader _levelLoader;
 
     public override void InstallBindings()
     {
         Container.Bind<Player>().FromInstance(_player).AsSingle();
-        Container.Bind<GameState>().FromInstance(_gameState).AsSingle();
         Container.Bind<LevelLoader>().FromInstance(_levelLoader).AsSingle();
     }
 }
